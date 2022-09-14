530cd10fb71d5eb449635e0e3db895b940dd53d2
Douglas Berdeaux <weaknetlabs@gmail.com>
2022-09-14 11:25:34 -0400

    Added --truncate functionality

--- misc/burp/BurpExportURIs.py
+++ misc/burp/BurpExportURIs.py
@@ -12,10 +12,16 @@ def usage(msg):
     sys.exit(1)
 
 def main(): ## Are we running as a script?
-  if len(sys.argv) != 2: ## Requires a filename as an argument
+  if not len(sys.argv) > 1: ## Requires a filename as an argument
       usage("No Arguments.")
   else:
       export_file = sys.argv[1] ## grab the file
+      truncate = False
+      if len(sys.argv) == 3:
+          if sys.argv[2] == "--truncate":
+              truncate = True
+          else:
+              usage(f"Not sure what {sys.argv[2]} is.") ## what was given to me lol
       if os.path.exists(export_file):
           with open(export_file) as burp_file:
               unique_uris = [] ## Storage for unique URIs
@@ -23,6 +29,8 @@ def main(): ## Are we running as a script?
                   ## FORMAT: <url><![CDATA[https://site.nope/nope?id=nope&nope=nope]]></url>
                   if re.search(r'<url><!.CDATA.http',line): ## We have a URI:
                       uri = re.sub(r'^.*(http[^\]]+).*',r'\1',line).strip()
+                      if truncate: ## truncate it
+                         uri = re.sub(r'^https?...[^/]+','',uri) ## sed -r 's/^https?...[^/]+\//\//g'
                       if uri not in unique_uris:
                           unique_uris.append(uri)
               for uri in unique_uris:
