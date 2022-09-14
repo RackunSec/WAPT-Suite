e994c03de6e0bb122a72c7ef52486968ff95cca6
Douglas Berdeaux <weaknetlabs@gmail.com>
2022-09-14 09:23:17 -0400

    Update wwwordlist.py

--- misc/wordlists/wwwordlist.py
+++ misc/wordlists/wwwordlist.py
@@ -5,49 +5,57 @@
 import requests
 import sys
 import re
+import urllib3 ## Used to weed out the SSL/TLS warnings
+urllib3.disable_warnings()
+
 def usage():
     print("Usage: python3 wwwlist.py (URI) | tee file.txt")
     sys.exit()
-if(len(sys.argv)!=2):
-    usage()
-else:
-    unique_words = [] # Empty list
-    uri = sys.argv[1] # Argument URI
-    if re.match(r"^http(s)?:.*",uri):
-        # here I set the User Agent for the Requests:
-        headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:91.0) Gecko/20100101 Firefox/91.0"}
-        #print(f"[i] Scraping URI: {uri}") # DEBUG
-        try:
-            req_data = requests.get(uri,headers=headers)  ## Make the HTTP request and get the data
-        except:
-            print(f"[!] Could not access {uri}")
-            sys.exit()
-        #print(html)
-        for i,line in enumerate(req_data.iter_lines()):
-            decoded = line.decode("utf-8")
-            line_scrubbed = re.sub(r"<[^>]+>","",decoded) # Delete out all HTML tags
-            line_scrubbed = re.sub(r"^\s+","",line_scrubbed) # Remove all prepended white space
-            line_scrubbed = re.sub(r"&[a-z]+;"," ",line_scrubbed) # remove &nbsp; etc
-            line_scrubbed = re.sub(r"[^A-Za-z0-9_\s/-]","",line_scrubbed) # Remove Evrything that is not a word character ("_-" is okay)
-            if "/" in line_scrubbed:
-                line_array = line_scrubbed.split("/")
-                for line_item in line_array:
-                    if " " in line_item: # the line had a forward slash AND a space:
-                        line_item2 = line_item.split()
-                        for word2 in line_item2:
-                            if not re.match(r"^$",word2) and len(word2)!=1 and word2 not in unique_words: # remove blank lines and such
-                                unique_words.append(word2)
-                    else:
-                        if not re.match(r"^$",line_item) and len(line_item)!=1 and line_item not in unique_words: # remove blank lines and such
-                            unique_words.append(line_item)
-            else:
-                line_array = line_scrubbed.split() # split by whitespace
-                for line_item in line_array:
-                    if not re.match(r"^$",line_item) and len(line_item)!=1 and line_item not in unique_words: # remove blank lines and such
-                        unique_words.append(line_item)
-        unique_words.sort()
-        for word in unique_words:
-            print(word)
-    else:
-        print(f"[!] Not a Valid URI: {uri}")
+
+def main():
+    if(len(sys.argv)!=2):
         usage()
+    else:
+        unique_words = [] # Empty list
+        uri = sys.argv[1] # Argument URI
+        if re.match(r"^http(s)?:.*",uri):
+            # here I set the User Agent for the Requests:
+            headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:91.0) Gecko/20100101 Firefox/91.0"}
+            #print(f"[i] Scraping URI: {uri}") # DEBUG
+            try:
+                req_data = requests.get(uri,headers=headers,verify=False)  ## Make the HTTP request and get the data
+            except:
+                print(f"[!] Could not access {uri}")
+                sys.exit()
+            #print(html)
+            for i,line in enumerate(req_data.iter_lines()):
+                decoded = line.decode("utf-8")
+                if len(decoded) < 80: # get rid of hashes / etc
+                    line_scrubbed = re.sub(r"<[^>]+>","",decoded) # Delete out all HTML tags
+                    line_scrubbed = re.sub(r"^\s+","",line_scrubbed) # Remove all prepended white space
+                    line_scrubbed = re.sub(r"&[a-z]+;"," ",line_scrubbed) # remove &nbsp; etc
+                    line_scrubbed = re.sub(r"[^A-Za-z0-9_\s/-]","",line_scrubbed) # Remove Evrything that is not a word character ("_-" is okay)
+                    if "/" in line_scrubbed:
+                        line_array = line_scrubbed.split("/")
+                        for line_item in line_array:
+                            if " " in line_item: # the line had a forward slash AND a space:
+                                line_item2 = line_item.split()
+                                for word2 in line_item2:
+                                    if not re.match(r"^$",word2) and len(word2)!=1 and word2 not in unique_words: # remove blank lines and such
+                                        unique_words.append(word2)
+                            else:
+                                if not re.match(r"^$",line_item) and len(line_item)!=1 and line_item not in unique_words: # remove blank lines and such
+                                    unique_words.append(line_item)
+                    else:
+                        line_array = line_scrubbed.split() # split by whitespace
+                        for line_item in line_array:
+                            if not re.match(r"^$",line_item) and len(line_item)!=1 and line_item not in unique_words: # remove blank lines and such
+                                unique_words.append(line_item)
+            unique_words.sort()
+            for word in unique_words:
+                print(word)
+        else:
+            print(f"[!] Not a Valid URI: {uri}")
+            usage()
+if __name__ == "__main__":
+    main()
